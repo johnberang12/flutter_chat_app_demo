b8e93019edde31b6d89a454627837aa0505d3eb8
jober <jober.businesses@gmail.com>
2023-08-03 20:23:07 +0800

    playing with revert2

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -7,12 +7,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //Home is changed to Sizedbox
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SizedBox(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SizedBox());
   }
 }
