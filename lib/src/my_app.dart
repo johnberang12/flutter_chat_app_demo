5e8c0be20725d72fa48c50fcb5e4578ec9d1c6ba
jober <jober.businesses@gmail.com>
2023-08-03 20:31:57 +0800

    reverted back to original

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/src/features/authentication/signin_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Home is changed to Sizedbox
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SizedBox());
+        home: const SigninScreen());
   }
 }
